7cff1be018213828eaa6ea198791c93bb46dc207
Stefnotch <brandmairstefan@gmail.com>
2018-01-28 16:41:53 +0100

    More transparency tests
    Currently it has some sorting issues...

--- TimeNexus/TimeNexus.Game/Effects/Shader1.cs
+++ TimeNexus/TimeNexus.Game/Effects/Shader1.cs
@@ -98,7 +98,6 @@ namespace SiliconStudio.Xenko.Rendering
                     {
                         context.Mixin(mixin, (extensionPixelStageSurfaceFilter));
                     }
-                    context.Mixin(mixin, "BShader");
                     if (context.ChildEffectName == "GBuffer")
                     {
                         context.Mixin(mixin, "GBuffer");
@@ -121,6 +120,7 @@ namespace SiliconStudio.Xenko.Rendering
                     context.Mixin(mixin, "ShadowMapCasterCubeMap");
                     return;
                 }
+                context.Mixin(mixin, "BShader");
             }
 
             [ModuleInitializer]
