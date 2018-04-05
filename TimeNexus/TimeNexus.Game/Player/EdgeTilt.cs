190ee13edb115b48f01bf661eb9218a84a0d9db6
Unknown <arno.bluemel@gmail.com>
2018-04-05 10:39:42 +0200

    Etwas

--- TimeNexus/TimeNexus.Game/Player/EdgeTilt.cs
+++ TimeNexus/TimeNexus.Game/Player/EdgeTilt.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TimeNexus.Debug;
+//using TimeNexus.Debug;
 
 namespace TimeNexus.Player
 {
