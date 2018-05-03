edb9408fc81a97a93d9b83e5f5e1e7fc594502e2
Unknown <arno.bluemel@gmail.com>
2018-05-03 11:44:34 +0200

    Created Pauseroom
    Inserted a little room into the Root-Scene and (hopefully) layed the foundation for the Pause-Menu.

---
+++ TimeNexus/TimeNexus.Game/Menu/ButtonTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Input;
+using SiliconStudio.Xenko.Engine;
+using TimeNexus.Objects;
+using System.Reactive.Linq;
+using TimeNexus.Player;
+using TimeNexus.ExtensionMethods;
+
+namespace Menu
+{
+    public class ButtonTrigger : SyncScript
+    {
+        private const int MAX_DIST = 10;
+        public TriggerAction action;
+
+        private static Vector3 GetPlayerPos
+        {
+            get => PlayerController.Player.Transform.GetWorldPosition();
+        }
+
+        public override void Start()
+        {
+            this.Entity.GetOrCreate<LookatTrigger>().OnLookatStart.Where(hitRes => (hitRes.Point - GetPlayerPos).Length() < MAX_DIST);
+            
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    public abstract class TriggerAction : ScriptComponent
+    {
+        public abstract void Action();
+    }
+}
---
+++ TimeNexus/TimeNexus.Game/Menu/Resume.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiliconStudio.Core.Mathematics;
+using SiliconStudio.Xenko.Input;
+using SiliconStudio.Xenko.Engine;
+
+namespace Menu
+{
+    public class Resume : TriggerAction
+    {
+        public override void Action()
+        {
+            Console.WriteLine("Resume");
+        }
+    }
+}
